82fada3eee4f1f2ec46bf08f6bbfa3502981dfff
Yu Zhou <71795670+audreyeternal@users.noreply.github.com>
2023-09-20 18:59:33 +0200

    Update resnet_torch.py
    remove dynamic shape.

--- cellpose/resnet_torch.py
+++ cellpose/resnet_torch.py
@@ -147,7 +147,7 @@ class make_style(nn.Module):
 
     def forward(self, x0):
         #style = self.pool_all(x0)
-        style = F.avg_pool2d(x0, kernel_size=(x0.shape[-2],x0.shape[-1]))
+        style = F.avg_pool2d(x0, kernel_size=(28,28))
         style = self.flatten(style)
         style = style / torch.sum(style**2, axis=1, keepdim=True)**.5
 
